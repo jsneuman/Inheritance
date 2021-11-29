11217d67951679c71b7d5272418a08ef2c8bebcb
jsneuman <jsneuman2@gmail.com>
2021-11-28 22:38:48 -0500

    Created main function and called displayInfo for all classes

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -1,7 +1,19 @@
+#include "Car.h"
+#include "Truck.h"
+#include "Vehicle.h"
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	Vehicle vehicle;
+	Car car;
+	Truck truck;
 
+	cout << "Vehicle Program" << endl << endl;
+	vehicle.displayInfo();
+	car.displayInfo();
+	truck.displayInfo();
+
+	return 0;
 }
